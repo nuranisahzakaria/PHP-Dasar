4df61290fa45c0c0c03e0e44174a0804d2fce6a3
nessaaa <nnuranisahh@gmail.com>
2022-06-30 22:36:31 +0700

    Ulang-ulang, belum mulai tadi. Karna gagal terus upload di github. MULAI DARI SEKARANG YA PROGGRESNYA.

--- Database12/Database buku/index.php
+++ Database12/Database buku/index.php
@@ -1,12 +1,42 @@
+<?php 
+    require 'koneksi.php';
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Data Buku</title>
+    <title>Daftar Buku</title>
 </head>
 <body>
-    
+    <a href="tambah.php">TAMBAH DATA BUKU</a>
+    <table border="2" cellpadding="10px">
+        <tr>
+            <th>No</th>
+            <th>Judul Buku</th>
+            <th>Harga</th>
+            <th>Gambar</th>
+            <th>Status</th>
+            <th>Aksi</th>
+        </tr>
+        <tr>
+            <?php $i = 1;  ?>
+            <?php while ($isi = mysqli_fetch_assoc($result)): ?>
+
+            <td><?php echo $i++; ?></td>
+            <td><?php echo $isi["nama_menu"]; ?></td>
+            <td><?php echo $isi["harga"]; ?></td>
+            <td><img src="img/<?php echo $isi['gambar'] ?>" width="50px"></td>
+            <td><?php echo $isi['status'] == 0? 'Tidak Tersedia':'Tersedia';?></td>
+            <td>
+                <a href="edit.php?id=<?php echo $row ["id"] ?>">Edit</a>|
+                <a href="hapus.php?id=<?php echo $row ["id"] ?>"
+                onclick="return confirm ('Apakah Anda Ingin Menghapus Data Ini');">Hapus</a>
+            </td>
+        </tr>
+
+        <?php endwhile; ?>
+    </table>
 </body>
 </html>
